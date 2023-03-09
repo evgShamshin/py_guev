daa87b194e027d3ca6ac5b447be8c37874d2a684
e.shamshin <e.shamshin@ADMINISTRATOR>
2023-03-10 00:25:16 +0300

    2023.03.09

---
+++ string_data_type/indexing/num_1.py
@@ -0,0 +1,4 @@
+num, sum = input(), 0
+for i in range(len(num)):
+    sum += int(num[i])
+print(sum)
---
+++ string_data_type/indexing/num_2.py
@@ -0,0 +1,6 @@
+for i in input():
+    if i in '0123456789':
+        print('Цифра')
+        break
+else:
+    print('Цифр нет')
