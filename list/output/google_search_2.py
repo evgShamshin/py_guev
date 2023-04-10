4560866fe0d2ac2a8e75cc31c8f9b3fe2c1fd0e0
e.shamshin <e.shamshin@ADMINISTRATOR>
2023-04-10 20:40:03 +0300

    2023.04.10

--- list/output/google_search_2.py
+++ list/output/google_search_2.py
@@ -1,7 +1,15 @@
-num_1, _list_1 = int(input()), list()
-for _ in range(num_1):
-    _list_1.append(input())
-num_2, _list_2 = int(input()), list()
-for _ in range(num_2):
-    _list_2.append(input())
-if _list_2 in _list_1:
+#введенные строки
+num_1 = int(input())
+_list_1 = [input() for _ in range(num_1)]
+
+#поисковые запросы
+num_2 = int(input())
+_list_2 = [input() for _ in range(num_2)]
+
+for i_1 in _list_1:
+    count = 0
+    for i_2 in _list_2:
+        if i_2.lower() in i_1.lower():
+            count +=1
+            if count == num_2:
+                print(i_1)
